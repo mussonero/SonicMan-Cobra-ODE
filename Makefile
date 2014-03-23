8a8f3f9d9e8e8b7b133305baf8e2ebd089cdb6a1
mussonero@gmail.com <mussonero@gmail.com>
2014-03-23 20:07:40 +0100

    W I P MultiSonic

---
+++ Makefile
@@ -0,0 +1,85 @@
+CELL_MK_DIR = $(CELL_SDK)/samples/mk
+include $(CELL_MK_DIR)/sdk.makedef.mk
+CELL_INC_DIR = $(CELL_SDK)/target/include
+
+MM	= source/
+VIDEO	= video/
+FTP	= openftp/
+SHADERS = shaders/
+RELEASE = ./release
+BIN	= bin/
+NPDRM	= /NPDRM_RELEASE
+
+MM_REL	= MultiSonic
+APPID	= BCED01063
+
+MAKE_SELF_WC = make_self_wc
+
+CONTENT_ID=MM4PS3-$(APPID)_00-MULTIMANAGER0209
+
+PPU_SRCS = $(MM)graphics.cpp $(MM)language.cpp 
+PPU_SRCS += $(VPSHADER_PPU_OBJS) $(FPSHADER_PPU_OBJS) 
+PPU_SRCS +=	$(MM)$(VIDEO)vpshader.vp \
+		$(MM)$(VIDEO)fpshader.fp \
+		$(MM)$(VIDEO)util.c \
+		$(MM)$(VIDEO)video.c \
+		$(MM)$(VIDEO)common.c \
+		$(MM)$(VIDEO)avidmux.c \
+		$(MM)$(VIDEO)vdec.c \
+		$(MM)$(VIDEO)vpost.c \
+		$(MM)$(VIDEO)vdisp.c \
+		$(MM)$(VIDEO)adec.c \
+		$(MM)$(VIDEO)apost.c \
+		$(MM)$(VIDEO)amixer.c \
+		$(MM)$(VIDEO)avsync.c
+
+PPU_SRCS +=	$(MM)$(FTP)ftp.c $(MM)$(FTP)ftpcmd.c $(MM)$(FTP)functions.c 
+
+PPU_SRCS += $(MM)multiman.cpp $(MM)manager.cpp $(MM)peek_poke.cpp $(MM)mm.cpp $(MM)hvcall.cpp $(MM)syscall36.cpp $(MM)syscall8.c $(MM)fonts.c $(MM)fonts_render.c $(MM)mscommon.cpp
+
+PPU_TARGET = $(MM_REL)_BARE.elf EBOOT.BIN
+
+PPU_OPTIMIZE_LV := -O2 -fno-exceptions
+
+# Disable compiler "warning: format not a string literal, argument types not checked" (not a good idea)
+PPU_CPPFLAGS	:= -Wformat=0
+
+PPU_INCDIRS= -Iinclude -I$(CELL_INC_DIR) -I$(CELL_INC_DIR)/usb/usbpad -I$(CELL_INC_DIR)/usb/usbkb -I$(CELL_SDK)/target/ppu/include/sysutil -I$(CELL_SDK)/target/ppu/include 
+PPU_LDLIBS = -lvpost_stub -lvdec_stub
+PPU_LDLIBS += -lfont_stub -lfontFT_stub -lfreetype_stub -lpthread -lmixer -lm -lmstreamSPURSMP3 -ladec_stub -laudio_stub -lnet_stub -lnetctl_stub -lpngdec_stub -ldbgfont_gcm -lgcm_cmd -lgcm_sys_stub -lio_stub -lsysmodule_stub -lsysutil_stub -lfs_stub -lhttp_util_stub  -lspurs_stub -ljpgdec_stub -lhttp_stub -lsysutil_music_export_stub -lsysutil_photo_export_stub -lsysutil_video_export_stub -lrtc_stub -lsysutil_screenshot_stub -lsysutil_np_stub
+PPU_LDLIBS += -lusbd_stub
+
+
+PPU_LDLIBS += -l./libpmsd -l./libpfs -l./libpfsm
+
+all : $(PPU_TARGET)
+	@scetool --sce-type SELF --compress-data FALSE --self-type APP --key-revision 0004 --self-fw-version 0003004100000000 --self-app-version 0001000000000000 --self-auth-id 1010000001000003 --self-vendor-id  01000002 --self-cap-flags 00000000000000000000000000000000000000000000003b0000000100040000 -e multiMAN2_BARE.elf EBOOT.BIN
+	
+
+
+PPU_CFLAGS  += -g -O2 -fno-exceptions 
+
+VPSHADER_SRCS = vpshader.cg vpshader2.cg
+FPSHADER_SRCS = fpshader.cg fpshader2.cg
+
+VPSHADER_PPU_OBJS = $(patsubst %.cg, $(OBJS_DIR)/$(MM)%.ppu.o, $(VPSHADER_SRCS))
+FPSHADER_PPU_OBJS = $(patsubst %.cg, $(OBJS_DIR)/$(MM)%.ppu.o, $(FPSHADER_SRCS))
+
+include $(CELL_MK_DIR)/sdk.target.mk
+
+PPU_OBJS += $(VPSHADER_PPU_OBJS) $(FPSHADER_PPU_OBJS)
+
+$(VPSHADER_PPU_OBJS): $(OBJS_DIR)/$(MM)%.ppu.o : %.vpo
+	@mkdir -p $(dir $(@))
+	@$(PPU_OBJCOPY)  -I binary -O elf64-powerpc-celloslv2 -B powerpc $< $@ > nul
+
+$(FPSHADER_PPU_OBJS): $(OBJS_DIR)/$(MM)%.ppu.o : %.fpo
+	@mkdir -p $(dir $(@))
+	@$(PPU_OBJCOPY)  -I binary -O elf64-powerpc-celloslv2 -B powerpc $< $@ > nul
+
+
+pkg : $(PPU_TARGET)
+	@mkdir -p $(BIN)
+	@$(PPU_STRIP) -s $< -o $(OBJS_DIR)/$(PPU_TARGET)
+	@$(MAKE_FSELF_NPDRM) $(PPU_TARGET) $(RELEASE)$(NPDRM)/USRDIR/EBOOT.BIN
+	$(MAKE_PACKAGE_NPDRM) $(RELEASE)/package.conf $(RELEASE)$(NPDRM)
---
+++ include/manager.h
@@ -0,0 +1,152 @@
+/* COBRA manager parsing library. v1.7
+ * This software is released into the public domain.
+ */
+#ifndef _MANAGER_H_
+#define _MANAGER_H_
+
+#include <ppu-types.h>
+#include <lv2/sysfs.h>
+
+/*
+ * This library provides an API to parse the available ISO files
+ * provided by the COBRA ODE.
+ * You can communicate with the COBRA ODE firmware by accessing those
+ * files. Please refer to user manual for more information.
+ *
+ * You must allocate a CobraManager structure and call cobra_manager_init to get
+ * a list of all valid ISO files and other information.
+ * It will fill the structure and return the number of games found.
+ * You must call cobra_manager_free to free resources when done.
+ * You can use cobra_iso_run to tell COBRA ODE to select an iso.
+ * Game dumping requires post-processing that needs to be done via the appropriate API.
+ * You can create a CobraDisc structure with cobra_disc_open, then use
+ * cobra_disc_write to copy 64KB of the iso into an output file.
+ * It will return the number of bytes written or 0 in case of error or EOF.
+ * Use cobra_disc_close to close and free the CobraDisc structure.
+ *
+ * Example of use :
+ *
+  CobraManager manager;
+  int i;
+
+  cobra_manager_init(&manager);
+
+  // Print firmware version
+  printf ("COBRA ODE Firmware : v%d.%d\n", manager.info.major, manager.info.minor);
+
+  // Parse all ISO files
+  for (i = 0; i < manager.num_iso; i++)
+  {
+    if (manager.iso[i].sfo_path != NULL)
+    parse_sfo_file(manager.iso[i].sfo_path);
+    if (manager.iso[i].png_path != NULL)
+      load_png_from_file(manager.iso[i].png_path);
+    show_filename(manager.iso[i].filename);
+    show_iso_type(manager.iso[i].type);
+    if (manager.info.major > 1 || manager.info.minor >= 3)
+      show_iso_size(manager.iso[i].size);
+  }
+
+  // User selects game
+  cobra_iso_run(&manager.iso[selection]);
+
+  // User dumps disc
+  CobraDisc *disc = cobra_disc_open(&manager);
+  s32 out;
+
+  sysFsOpen ("/dev_usb000/DISC.ISO", SYS_O_WRONLY, &out, NULL, 0);
+
+  while (1)
+  {
+    int bytes = cobra_disc_write (disc, out);
+    // Show progress
+    // ...
+    //////
+    if (bytes == 0)
+      break;
+  }
+  sysFsClose (out);
+  cobra_disc_close (disc);
+
+  // Eject and load game
+  cobra_manager_eject(&manager);
+
+  // Exit
+  cobra_manager_free(&manager);
+
+ *
+ */
+
+#define MAX_ISO 500
+#define MAX_FILENAME_LENGTH 128
+#define DEV_BDVD "/dev_bdvd"
+#define COBRA_PATH DEV_BDVD "/COBRA"
+
+enum {
+  COBRA_ISO_TYPE_PASSTHROUGH = 0, // Since 2.0 - If PT type is available, DISC.ISO will not be corrupted
+  COBRA_ISO_TYPE_PS1_GAME = 1,
+  COBRA_ISO_TYPE_PS2_GAME = 2,
+  COBRA_ISO_TYPE_PS3_GAME = 3,
+  COBRA_ISO_TYPE_BD_MOVIE = 4,
+  COBRA_ISO_TYPE_DVD_MOVIE = 5,
+};
+
+typedef struct {
+  char game_id[10];
+  char *filename;
+  char volume_name[32]; // Since 1.9
+  u64 size; // Since 1.3
+  u8 type; // Since 1.9
+  char *png_path; // NULL if iso is not a PS3 game or has no ICON0.PNG
+  char *sfo_path; // NULL if iso not a PS3 game;
+  char run_path[MAX_FILENAME_LENGTH+1];
+} CobraIso;
+
+typedef struct {
+  u8 major;
+  u8 minor; // 1.0 and 1.1 appear as 1.0
+  u32 disc_lba; // Since 1.3
+  u8 quick_eject; // Since 1.6
+} __attribute__ ((packed)) CobraInfo;
+
+typedef struct {
+  CobraIso iso[MAX_ISO];
+  int num_iso;
+  char disc_iso[MAX_FILENAME_LENGTH+1];
+  char *eject;
+  CobraInfo info;
+} CobraManager;
+
+typedef struct {
+  s32 fd;
+  u32 disc_lba;
+  u32 sector;
+  struct {
+    u32 num_plain;
+    u32 zero;
+    struct {
+      u32 start;
+      u32 end;
+    } plain_regions[255];
+  } sector0;
+  u8 buffer[32 * 2048];
+} CobraDisc;
+
+// Parse the manager files and generate the listing
+int cobra_manager_init (CobraManager *manager);
+// Free resources
+void cobra_manager_free (CobraManager *manager);
+// Eject manager disc and load game
+u8 cobra_manager_eject (CobraManager *manager);
+// Run the selected iso
+u8 cobra_iso_run (CobraIso *iso);
+// Open DISC.ISO for dumping. Requires MCU firmware v1.3 or later
+CobraDisc * cobra_disc_open (CobraManager *manager);
+// Read 32 sectors from DISC.ISO and write them to the output file.
+// Returns number of bytes written or 0 in case of error or EOF
+u32 cobra_disc_write (CobraDisc *disc, s32 output);
+// Close file and free resources
+void cobra_disc_close (CobraDisc *disc);
+
+
+#endif
